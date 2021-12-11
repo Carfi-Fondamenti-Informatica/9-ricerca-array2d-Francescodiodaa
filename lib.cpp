7d097744235062697e2c505f4bf1e33ae701788c
Francescodiodaa <91884945+Francescodiodaa@users.noreply.github.com>
2021-12-11 10:58:28 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,36 @@
+//
+// Created by Francesco Dioda on 10/12/21.
+//
+#include <iostream>
+#include "lib.h"
+#include <cstring>
+using namespace std;
+void controllomaiuscola1(char arr1[10][20], char arr2[10]){
+    for(int i=0;i<10;i++){
+        for(int j=0;j<20;j++){
+            if((int)arr1[i][j]<=90){
+                arr1[i][j]=tolower(arr1[i][j]);
+            }
+        }
+    }
+
+    for(int i=0;i<10;i++){
+        if((int)arr2[i]<=90){
+            arr2[i]=tolower(arr2[i]);
+
+        }}}
+void confronto(char a1[10][20],char a2[10],int c) {
+    for(int i=0;i<=10;i++) {
+        if (strcmp(a2,a1[i]) == 0) {
+            c=1;
+            cout<<i+1;
+            break;
+        }
+
+    }
+    if (c!=1){
+        cout<<"non presente";
+    }
+}
+
 
