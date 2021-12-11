9c489104d4b8c062ef7cbc1efc58709b4b9d5465
Francescodiodaa <91884945+Francescodiodaa@users.noreply.github.com>
2021-12-11 10:57:56 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,10 @@
+//
+// Created by Francesco Dioda on 10/12/21.
+//
+
+#ifndef ESERCITAZIONEFINALE_LIB_H
+#define ESERCITAZIONEFINALE_LIB_H
+void controllomaiuscola1(char arr1[10][20], char arr2[10]);
+void confronto(char a1[10][20],char a2[10],int c);
+#endif //ESERCITAZIONEFINALE_LIB_H
 
