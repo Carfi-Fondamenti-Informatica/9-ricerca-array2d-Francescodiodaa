440da841c430f3f5b6847ad662d92a604b016dfd
Francescodiodaa <91884945+Francescodiodaa@users.noreply.github.com>
2021-12-11 10:58:11 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include "string.h"
+#include "lib.h"
 
-int main(){
-  return 0;
-}
+using namespace std;
+int main() {
+char array[10][20];
+char mat[20];
+        for (int i = 0; i <10; i++) {
+            cin >> array[i];
+
+        }
+        for(int i=0;i<1;i++){
+            cin>>mat;
+        }
+        controllomaiuscola1(array,mat);
+        confronto(array,mat,0);
+
+
+    return 0;
+    }
+
+
